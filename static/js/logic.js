a4ef69871c26fe7680b7a99a850afceaa576fcc7
Sunwoo Kim <s.kim32415@gmail.com>
2021-07-23 21:35:02 -0700

    added starting coordinates and map zoom level

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,12 +4,7 @@ var mapZoomLevel = 3;
 
 // Create the createMap function
 function createMap(newYorkCoords, mapZoomLevel, bikeStations) {
-  // var myMap L.map("map", {
-  //   center: newYorkCoords,
-  //   zoom: mapZoomLevel
-  // });
-
-    // Create the tile layer that will be the background of our map
+  // Create the tile layer that will be the background of our map
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
