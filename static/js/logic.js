e9983960e3dbacffb9331201dbec042fa310331a
Sunwoo Kim <s.kim32415@gmail.com>
2021-07-23 22:07:12 -0700

    added tile layers from mapbox

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,22 +3,31 @@ var startingCoords = [37.09, -95.71];
 var mapZoomLevel = 3;
 
 // Create the createMap function
-function createMap(newYorkCoords, mapZoomLevel, bikeStations) {
-  // Create the tile layer that will be the background of our map
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+function createMap(startingCoords, mapZoomLevel, earthquakes) {
+
+  // Create a baseMaps object to hold the satellite layer
+  var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
-    id: "mapbox/streets-v11",
+    id: "satellite-v9",
     accessToken: API_KEY
   });
 
-    // Create a baseMaps object to hold the lightmap layer
+  // Create a baseMaps object to hold the darkmap layer
   var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "dark-v10",
+    accessToken: API_KEY
+  });
+
+  // Create a baseMaps object to hold the outdoormap layer
+  var outdoormap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "outdoors-v11",
     accessToken: API_KEY
   });
 
@@ -28,7 +37,7 @@ function createMap(newYorkCoords, mapZoomLevel, bikeStations) {
   };
   // Create an overlayMaps object to hold the bikeStations layer
   overlayMaps = {
-    Bike: bikeStations
+    Earthquakes: earthquake
   };
 
   // Create the map object with options
